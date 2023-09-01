a77d9d759eae711120d15e45a811394761f84833
Sushma <cs21sushma@svpkumta.in>
2023-09-01 12:54:32 +0530

    adding files

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Java program to print a message
+
+public class HelloWorld {
+    public Static void main (String args[]){
+        System.out.println("Hello World");
+        System.out.print("Welcome to Object Oriented Programming");
+        System.out.println("and Design with Java");
+    }
+}
